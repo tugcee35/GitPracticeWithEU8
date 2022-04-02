d4b6fe36d5fd776b18e056db46e361d9de550c5a
tugcee35 <100022500+tugcee35@users.noreply.github.com>
2022-04-02 22:19:36 +0300

    edge test

--- src/day01/TestCase01.java
+++ src/day01/TestCase01.java
@@ -4,5 +4,9 @@ public class TestCase01 {
 
     public static void main(String[] args) {
         System.out.println("Testing with Chrome Browser");
+
+        System.out.println("Testing with Safari");
+
+        System.out.println("Testing with Edge");
     }
 }
